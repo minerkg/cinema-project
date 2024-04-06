3f5cc764ed4213022a16d544a8ade91a93d45795
Alexandru Mucenic <41705755+AlexandruMucenic@users.noreply.github.com>
2024-04-06 15:03:53 +0300

    added client card class (#4)
    Co-authored-by: Mius <mius@mius-mbp.cs.ubbcluj.ro>

--- src/main/java/ro/ubb/cinema/domain/ClientCard.java
+++ src/main/java/ro/ubb/cinema/domain/ClientCard.java
@@ -1,5 +1,82 @@
 package ro.ubb.cinema.domain;
 
-public class ClientCard {
-    // TODO: CRUD card client: id, nume, prenume, CNP, data nașterii (dd.mm.yyyy), data înregistrării (dd.mm.yyyy), puncte acumulate. CNP-ul trebuie să fie unic.
+import java.time.LocalDate;
+
+public class ClientCard extends BaseEntity {
+    private String firstName;
+    private String lastName;
+    private int cnp;
+    private LocalDate birthDate;
+    private LocalDate registeringDate;
+    private int loyaltyPoints;
+
+    public ClientCard(int id, String firstName, String lastName, int cnp, LocalDate birthDate, LocalDate registeringDate, int loyaltyPoints) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.cnp = cnp;
+        this.birthDate = birthDate;
+        this.registeringDate = registeringDate;
+        this.loyaltyPoints = loyaltyPoints;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getCnp() {
+        return cnp;
+    }
+
+    public void setCnp(int cnp) {
+        this.cnp = cnp;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public LocalDate getRegisteringDate() {
+        return registeringDate;
+    }
+
+    public void setRegisteringDate(LocalDate registeringDate) {
+        this.registeringDate = registeringDate;
+    }
+
+    public int getLoyaltyPoints() {
+        return loyaltyPoints;
+    }
+
+    public void setLoyaltyPoints(int loyaltyPoints) {
+        this.loyaltyPoints = loyaltyPoints;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientCard{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", cnp=" + cnp +
+                ", birthDate=" + birthDate +
+                ", registeringDate=" + registeringDate +
+                ", loyaltyPoints=" + loyaltyPoints +
+                '}';
+    }
 }
