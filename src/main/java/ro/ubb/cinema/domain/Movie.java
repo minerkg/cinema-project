7d35cf523c47a0ffa99fd49618ad90200f272b64
Alexandra Claudia Brad <106176985+AleBradC@users.noreply.github.com>
2024-04-06 14:58:29 +0300

    created movie and reservation class (#3)

--- src/main/java/ro/ubb/cinema/domain/Movie.java
+++ src/main/java/ro/ubb/cinema/domain/Movie.java
@@ -1,5 +1,58 @@
 package ro.ubb.cinema.domain;
 
-public class Movie extends BaseEntity{
-    // TODO: CRUD film: id, titlu, an apariție, preț bilet, în program. Prețul să fie strict pozitiv.
+public class Movie extends BaseEntity {
+    private String title;
+    private int year;
+    private String ticketPrice; // string pozitiv
+    private boolean inCinema;
+
+    public Movie(int id, String title, int year, String ticketPrice, boolean inCinema) {
+        super(id);
+        this.title = title;
+        this.year = year;
+        this.ticketPrice = ticketPrice;
+        this.inCinema = inCinema;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getTicketPrice() {
+        return ticketPrice;
+    }
+
+    public boolean isInCinema() {
+        return inCinema;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setTicketPrice(String ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
+
+    public void setInCinema(boolean inCinema) {
+        this.inCinema = inCinema;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "title='" + title + '\'' +
+                ", year=" + year +
+                ", ticketPrice='" + ticketPrice + '\'' +
+                ", inCinema=" + inCinema +
+                '}';
+    }
 }
--- src/main/java/ro/ubb/cinema/domain/Reservation.java
+++ src/main/java/ro/ubb/cinema/domain/Reservation.java
@@ -1,5 +1,51 @@
 package ro.ubb.cinema.domain;
 
-public class Reservation {
-    // TODO:  CRUD rezervare: id, id_film, id_card_client (poate fi nul), data și ora (dd.mm.yyyy HH:mm). Clientul acumulează pe card 10% (parte întreagă) din prețul filmului. Se tipărește numărul total de puncte de pe card. Rezervarea se poate face doar dacă filmul este încă în program.
+import java.time.LocalDate;
+
+public class Reservation extends BaseEntity {
+    // TODO: Clientul acumulează pe card 10% (parte întreagă) din prețul filmului. Se tipărește numărul total de puncte de pe card. Rezervarea se poate face doar dacă filmul este încă în program.
+
+    private int filmId;
+    private int clientCardId; // poate fii null
+    private LocalDate dayAndTime;
+
+    public Reservation(int id, int filmId, int clientCardId, LocalDate dayAndTime) {
+        super(id);
+        this.filmId = filmId;
+        this.clientCardId = clientCardId;
+        this.dayAndTime = dayAndTime;
+    }
+
+    public int getFilmId() {
+        return filmId;
+    }
+
+    public int getClientCardId() {
+        return clientCardId;
+    }
+
+    public LocalDate getDayAndTime() {
+        return dayAndTime;
+    }
+
+    public void setFilmId(int filmId) {
+        this.filmId = filmId;
+    }
+
+    public void setClientCardId(int clientCardId) {
+        this.clientCardId = clientCardId;
+    }
+
+    public void setDayAndTime(LocalDate dayAndTime) {
+        this.dayAndTime = dayAndTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "filmId=" + filmId +
+                ", clientCardId=" + clientCardId +
+                ", dayAndTime=" + dayAndTime +
+                '}';
+    }
 }
